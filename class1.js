8c7c800116eb3d0fcdbd386a9ebd1da5369ea0dd
saurabh kumar <saurabhkumar9493@gmail.com>
2024-11-07 16:01:16 +0530

    first commit

---
+++ class1.js
@@ -0,0 +1,16 @@
+// console.log(123)
+//console.log(456)
+// console.log(1+2)
+// console.log(784569+62268541)
+
+// console.log(456*23);
+// console.log(100/2)
+// ""
+// ''
+
+// console.log("neetu")
+// console.log('ganga')
+
+// console.log("amita",12, 'manmmet')
+
+
