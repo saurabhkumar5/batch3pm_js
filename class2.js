17b88df8bd171f5e176608df6c337a0f0ca554b1
saurabh kumar <saurabhkumar9493@gmail.com>
2024-11-08 16:00:17 +0530

    class2

---
+++ class2.js
@@ -0,0 +1,39 @@
+
+
+// var a = 10
+// var bcd = 20
+// console.log(bcd)
+
+
+
+// var neetu = 10
+// console.log(neetu)
+
+
+// var a123 = 20
+
+// var $a = 10
+// console.log($a)
+
+// var _a = 20
+// console.log(_a)
+
+
+// var hello_how_are_you = 20
+// var kullluMbanali = 30
+
+// var a = 10
+// var A = 20
+// console.log(A)
+
+
+// var a = "neetu"
+// console.log(a)
+
+// var a = 'd'
+// console.log(a)
+
+// let a=20
+// console.log('a')
+
+console.log('abfhh')
