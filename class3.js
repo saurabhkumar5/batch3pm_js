fcd58af1cb0fd0b628e9f8f9330b4b1c8997c804
saurabh kumar <saurabhkumar9493@gmail.com>
2024-11-11 16:37:43 +0530

    logical

---
+++ class3.js
@@ -0,0 +1,22 @@
+
+    //   let a = 45
+        //   a = a+10  
+             //a+=10  //a = a+10
+
+
+            //  a = a+1
+            //    a+=1
+                //   a++
+                //   a--  // a = a-1
+        //   console.log(a)
+
+
+
+        //    let a = 10
+        //        b = a+1
+        //        console.log(b)
+
+            // let a = 10
+            //     //    ++a 
+            //        a++
+            //     console.log(a)
---
+++ class4.js
@@ -0,0 +1,26 @@
+ 
+
+//  console.log(5>=5)
+// console.log(41<=37)
+
+// console.log(12>=8)
+
+// let a = 10
+// let b = 20
+// console.log(a>b)
+// console.log(a+b)
+
+
+
+// logical operator
+// 1. logical && And operator
+// 2. logical ||  OR operator
+// 3. logical ! NOT operator
+
+
+
+//    console.log(4>2 && 5>3 && 8>15)
+
+// console.log(10>50 || 6>30 || 4>12)
+
+    // console.log(!(4>20))
