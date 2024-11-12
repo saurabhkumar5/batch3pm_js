5a884168da6e4f2e42f1f2974a329c30b6abadce
saurabh kumar <saurabhkumar9493@gmail.com>
2024-11-12 16:11:11 +0530

    condition

---
+++ class5.js
@@ -0,0 +1,59 @@
+// 1. only if
+// 2. if-else
+// 3. else if
+// 4. nested if else
+
+
+// 1. only if
+
+    //  if(condition){
+    //   // code
+    //  }
+
+
+
+//    let age = 10
+//    if(age>=10){
+//     console.log("you can vote")
+//    }
+
+
+// let age = 10
+
+// if(age>5){
+//     console.log("hello")
+// }
+
+
+// if(40>80){
+//     console.log("kullu")
+// }
+
+
+// if(41>30){
+//     console.log("shimla")
+// }
+
+
+
+
+// if(25>10){
+//     console.log("kullu")
+// } else{
+//     console.log("manali")
+// }
+
+
+// let age = 18
+// if(age>=18){
+//     console.log("you can vote")
+// }else{
+//     console.log("you cant vote")
+// }
+
+
+if(10>5 && 12>5){
+    console.log("hello")
+}else{
+    console.log("byee")
+}
