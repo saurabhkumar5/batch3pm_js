cf96bdefa472e35e4b55acc3a0bcae6889c84c1a
saurabh kumar <saurabhkumar9493@gmail.com>
2024-11-13 16:05:09 +0530

    condition

--- class5.js
+++ class5.js
@@ -52,8 +52,122 @@
 // }
 
 
-if(10>5 && 12>5){
-    console.log("hello")
-}else{
-    console.log("byee")
+// if(10>5 && 12>5){
+//     console.log("hello")
+// }else{
+//     console.log("byee")
+// }
+
+
+
+// if(4>3 && 10>9){
+//     console.log("hello")
+// }else{
+//     console.log("byee")
+// }
+
+// if(4>8 || 12>90){
+//     console.log("hello")
+// }else{
+//     console.log("byee")
+// }
+
+
+// let age = 25
+// if(age>18){
+//     console.log("hello")
+// }
+// if(age>240){
+//     console.log("world")
+// }else{
+//     console.log("byee")
+// }
+
+
+
+// let a = 10
+// if(a>15){
+//     console.log("kullu")
+// }
+// if(a>20){
+//     console.log("manali")
+// }
+// if(a>22){
+//     console.log("shimla")
+// }else{
+//     console.log("byee")
+// }
+
+
+
+// if(20>150){
+//     console.log("hello")
+// }
+// else if(12>10){
+//     console.log("world")
+// }
+
+
+
+// if(10>80){
+//     console.log("hello")
+// }
+// else if(15>120){
+//     console.log("world")
+// }
+// else if(20>100){
+//     console.log("byee")
+// }else{
+//     console.log("kullu")
+// }
+
+
+
+
+
+// 4. nested if-else
+
+//    if(40>200){
+//         if(10>50){
+//             console.log("hello")
+//         }else{
+//             console.log("world")
+//         }
+//    }
+//    else{
+//     console.log("byee")
+//    }
+
+
+
+
+// if(10>5){
+//     if(45>50){
+//         console.log("kullu")
+//     }
+//     else{
+//         console.log("manali")
+//     }
+// }
+// else{
+//     console.log("byee")
+// }
+
+
+
+if(5>6){
+    if(12>10){
+        console.log("hello")
+    }
+    else{
+        console.log("world")
+    }
+}
+else{
+    if(8>10){
+        console.log("kullu")
+    }
+    else{
+        console.log("byeee")
+    }
 }
