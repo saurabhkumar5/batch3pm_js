ee29fe8d4ec6ec6dbbad69e77e96579791c31bc5
saurabh kumar <saurabhkumar9493@gmail.com>
2024-11-18 16:04:50 +0530

    string

---
+++ class6.js
@@ -0,0 +1,18 @@
+// 1.Math.abs()
+// 2.Math.floor()
+// 3.Math.ceil()
+// 4.Math.pow()
+// 5.Math.random()
+
+
+// console.log(Math.abs(-10.5))
+// console.log(Math.floor(10.2))
+// console.log(Math.ceil(8.1))
+// console.log(Math.pow(3,3))
+// console.log(Math.random()*10)
+
+
+// console.log(Math.floor(Math.random()*10))
+
+
+
---
+++ class8.js
@@ -0,0 +1,8 @@
+// let a = 'hello' //string
+// let b = 10 //number
+
+// console.log(typeof(b))
+
+
+// let a = '@'
+// console.log(typeof(a))
