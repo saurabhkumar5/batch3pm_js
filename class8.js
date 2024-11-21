5b2090b80757c642aa59029e972caec04edc4eb1
saurabh kumar <saurabhkumar9493@gmail.com>
2024-11-21 15:44:15 +0530

    lol

--- class8.js
+++ class8.js
@@ -4,5 +4,58 @@
 // console.log(typeof(b))
 
 
+
 // let a = '@'
 // console.log(typeof(a))
+
+// let str = "anup"
+// // console.log(typeof(str))
+// // console.log(str.length)
+// console.log(str[3]) //indexing
+
+
+
+// let a = "kull u"
+// console.log(a[6])
+
+
+// let a = 10
+// let b = 20
+// console.log(a==b)
+
+// let name = "neetu"
+
+// // console.log(name[0])
+
+// if(name[0]=='s'){
+//     console.log("you can travell shimla")
+// }else{
+//     console.log("you cant travelll")
+// }
+
+
+// method 
+
+// 1. concat()
+
+//   let name1 = "neetu"
+//   let name2 = "ganga"
+// //   console.log(name1+"         "+name2)
+//   console.log(name2.concat(name1))
+
+
+
+// 2. indexOf()
+
+//   let a = "komal"
+//   console.log(a.indexOf('a'))
+
+// 3. replace()
+
+//    let a = "aman"
+//    console.log(a.replace('aman','raman'))
+
+// 4. search()
+
+// let str = "most welcome"
+//   console.log(str.search('t'))
