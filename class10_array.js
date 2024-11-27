9027f1ddafa467ddb8c7a90f6b48fae5593ad89d
saurabh kumar <saurabhkumar9493@gmail.com>
2024-11-27 16:11:34 +0530

    array

---
+++ class10_array.js
@@ -0,0 +1,45 @@
+// let a = 10
+// let b = 20
+// let c = 30 
+
+// let d = [10,20,30,40]
+// console.log(d)
+// console.log(d.length)
+// console.log(d[2])
+
+
+// 1. at()
+
+// let arr = [10,45,3,25]
+// console.log(arr.at(2))
+
+// let a = true+false
+// console.log(a)
+
+// Boolean data type => true/false
+
+// 2.push
+
+//  let arr = [10,20,30]
+//         arr.push(40)
+//         arr.push(50)
+//         arr.pop()
+//         arr.pop()
+//         arr.pop()
+//        console.log(arr)
+
+// let arr = []
+//      arr.push("mobile")
+//      arr.push("ring")
+//      arr.push("dress")
+//      arr.pop()
+// console.log(arr)
+
+// let arr = [10,20,30]
+//      arr.unshift(60)
+//      arr.unshift(70)
+//      arr.shift()
+//      arr.pop()
+//      console.log(arr)
+
+
